6ab6bd7bd761fdea9fcb1efc465d5a33fcd47138
HnKnA <nguyentronganhtuan2102@gmail.com>
2024-11-18 02:45:41 +0700

    Configure Suspense for admin response page

--- src/app/(authenticate)/admin/response/page.tsx
+++ src/app/(authenticate)/admin/response/page.tsx
@@ -1,150 +1,17 @@
-// "use client";
+"use client";
 
-// import Link from "next/link";
-// import { useRouter } from "next/navigation";
-// import { useEffect, useState, Suspense } from "react";
-// import { CircularProgress } from "@mui/material";
-// import { useSearchParams } from "next/navigation";
-// import "../../../globals.css";
+import { Suspense } from "react";
+import ResponsePageContent from "@/components/ResponsePageContent";
 
-// async function fetchResponseData(page = 1, pageSize = 7) {
-//   const res = await fetch(`/api/response?page=${page}&page_size=${pageSize}`);
-
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
-
-//   return res.json();
-// }
-
-// export default function ResponsePage() {
-//   const router = useRouter();
-//   const searchParams = useSearchParams();
-//   const [response, setResponse] = useState<any>(null);
-//   const page = parseInt(searchParams.get("page") || "1", 10);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const data = await fetchResponseData(page, 7);
-//       setResponse(data);
-//     };
-//     fetchData();
-//   }, [page]);
-
-//   // Log Out Handler
-//   const handleLogout = () => {
-//     setIsLoading(true);
-//     // Clear the isAdmin cookie
-//     document.cookie = "isAdmin=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-//     // Redirect to /admin
-//     router.replace("/");
-//   };
-
-//   if (!response) {
-//     return <p>Loading...</p>;
-//   }
-
-//   return (
-//     <>
-//       <title>Contact Response</title>
-//       <Suspense
-//         fallback={<h1 style={{ fontSize: "200px !important" }}>Loading...</h1>}
-//       >
-//         <section id="bricks">
-//           {/* Log Out Button */}
-//           <div
-//             style={{
-//               display: "flex",
-//               justifyContent: "flex-end",
-//               padding: "10px",
-//             }}
-//           >
-//             <button
-//               onClick={handleLogout}
-//               className="logout-button"
-//               disabled={isLoading}
-//             >
-//               {isLoading ? (
-//                 <CircularProgress size={24} color="inherit" />
-//               ) : (
-//                 "Log Out"
-//               )}
-//             </button>
-//           </div>
-
-//           <div className="row masonry">
-//             <div className="bricks-wrapper">
-//               <div className="grid-sizer"></div>
-
-//               {/* Response Table */}
-//               <div className="response-table-wrapper">
-//                 <table className="response-table">
-//                   <thead>
-//                     <tr>
-//                       <th>No</th>
-//                       <th>Name</th>
-//                       <th>Email</th>
-//                       <th>Website</th>
-//                       <th>Message</th>
-//                     </tr>
-//                   </thead>
-//                   <tbody>
-//                     {response.data.map((item: any) => (
-//                       <tr key={item.no}>
-//                         <td>{item.no}</td>
-//                         <td>{item.name}</td>
-//                         <td>{item.email}</td>
-//                         <td>{item.website}</td>
-//                         <td>{item.message}</td>
-//                       </tr>
-//                     ))}
-//                   </tbody>
-//                 </table>
-//               </div>
-//             </div>
-//           </div>
-
-//           {/* Pagination */}
-//           <div className="row">
-//             <nav className="pagination">
-//               {response.currentPage > 1 ? (
-//                 <Link
-//                   href={`/admin/response?page=${response.currentPage - 1}`}
-//                   className="page-numbers prev"
-//                 >
-//                   Prev
-//                 </Link>
-//               ) : (
-//                 <span className="page-numbers prev inactive">Prev</span>
-//               )}
-
-//               {Array.from({ length: response.totalPages }, (_, i) => (
-//                 <Link
-//                   key={i + 1}
-//                   href={`/admin/response?page=${i + 1}`}
-//                   className={`page-numbers ${
-//                     response.currentPage === i + 1 ? "current" : ""
-//                   }`}
-//                 >
-//                   {i + 1}
-//                 </Link>
-//               ))}
-
-//               {response.currentPage < response.totalPages ? (
-//                 <Link
-//                   href={`/admin/response?page=${response.currentPage + 1}`}
-//                   className="page-numbers next"
-//                 >
-//                   Next
-//                 </Link>
-//               ) : (
-//                 <span className="page-numbers next inactive">Next</span>
-//               )}
-//             </nav>
-//           </div>
-//         </section>
-//       </Suspense>
-//     </>
-//   );
-// }
+export default function Page() {
+  return (
+    <>
+      <title>Contact Response</title>
+      <Suspense
+        fallback={<h1 style={{ fontSize: "200px !important" }}>Loading...</h1>}
+      >
+        <ResponsePageContent />
+      </Suspense>
+    </>
+  );
+}
